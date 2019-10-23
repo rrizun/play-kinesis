6bb4c32980181b8e11c4b601fbe280a97d589fba
rrizun <rrizun@gmail.com>
2019-10-23 08:29:08 -0700

    checkpoint

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,6 +9,14 @@ public class Main {
 
     public static void main(String... args) throws Exception {
 
+            // ((ch.qos.logback.classic.Logger) org.slf4j.LoggerFactory.getLogger(ch.qos.logback.classic.Logger.ROOT_LOGGER_NAME).setLevel(level);
+
+
+        // org.slf4j.LoggerFactory.getILoggerFactory()
+
+        
+
+
         final String streamName = "TryKinesis-MyFirstStream63B28502-8AWS9GENXKPB";
 
         // kinesisClient.putRecord(PutRecordRequest.builder()
@@ -25,16 +33,17 @@ public class Main {
 
         KinesisProducerConfiguration kinesisProducerConfiguration = new KinesisProducerConfiguration();
             // AWSCredentials credentials = DefaultAWSCredentialsProviderChain.getInstance().getCredentials();
-        kinesisProducerConfiguration.setCredentialsProvider(DefaultAWSCredentialsProviderChain.getInstance());
-        kinesisProducerConfiguration.setVerifyCertificate(false);
+        // kinesisProducerConfiguration.setCredentialsProvider(DefaultAWSCredentialsProviderChain.getInstance());
+        // kinesisProducerConfiguration.setVerifyCertificate(false);
         kinesisProducerConfiguration.setRegion("us-east-1");
 
-        final KinesisProducer kinesis = new KinesisProducer();
+        final KinesisProducer kinesis = new KinesisProducer(kinesisProducerConfiguration);
         try {
             while (true) {
                 try {
-                    log("addUserRecord");
+                    log("addUserRecord[1]");
                     log(kinesis.addUserRecord(streamName, "myPartitionKey", ByteBuffer.wrap("myData".getBytes("UTF-8"))).get());
+                    log("addUserRecord[2]");
                 } finally {
                     // Thread.sleep(2000);
                 }
